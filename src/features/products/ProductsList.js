871cb9adb3c49692c018217f7057e20b37fda669
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2023-10-25 20:29:14 +0300

    PC

--- src/features/products/ProductsList.js
+++ src/features/products/ProductsList.js
@@ -1,18 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useGetProductsQuery } from './productsApiSlice'
 import Product from './Product'
 import ClipLoader from 'react-spinners/ClipLoader'
 import useAuth from '../../hooks/useAuth'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDown, faAngleUp, faFilter } from '@fortawesome/free-solid-svg-icons'
-import { AnimatePresence, motion } from 'framer-motion'
+import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons'
+import { motion } from 'framer-motion'
 import { useGetCategoriesQuery } from '../category/categoryApiSlice'
 import Category from '../category/Category'
 
 export default function ProductsList() 
 {
     const { id: userId } = useAuth()
-    const [filter, setFilter] = useState(false)
     const [categories, setCategories] = useState(false)
     const [selectedCategories, setSelectedCategories] = useState([])
     const [selectedProducts, setSelectedProducts] = useState([])
@@ -51,19 +50,35 @@ export default function ProductsList()
 
     useEffect(() => 
     {
-        let min = 99999999999
         let max = 0
-        selectedProducts.forEach(prod => 
+        let min = 99999999999
+        if(products?.ids && products?.entities)
         {
-            //@ts-ignore
-            if(Math.floor(prod.price) > max) max = Math.floor(prod.price)
-            //@ts-ignore
-            if(Math.floor(prod.price) < min) min = Math.floor(prod.price)
-        })
+            const { ids, entities } = products
+            ids.forEach(id => 
+                {
+                    if(Math.floor(entities[id].price) > max) max = Math.floor(entities[id].price)
+                    if(Math.floor(entities[id].price) < min) min = Math.floor(entities[id].price)
+                })
+        }
+
         setMaxPrice(max)
         setMinPrice(min)
         setPrice(min)
-    }, [selectedProducts])
+    }, [products])
+
+    useEffect(() => 
+    {
+        let min = 99999999999
+        selectedProducts.forEach(prod => 
+        {
+            //@ts-ignore
+            if(Math.floor(prod.price) < min) min = Math.floor(prod.price)
+        })
+        
+        setMinPrice(min)
+        if(price < min) setPrice(min)
+    }, [price, selectedProducts])
 
     useEffect(() => 
     {
@@ -87,68 +102,92 @@ export default function ProductsList()
         
     }, [selectedCategories, products])
 
-    console.log(selectedProducts)
+    const handlePriceChange = useCallback(() =>
+    {
+        const { ids, entities } = products
+        const prods = ids.map(id => entities[id])
+        const array = [...prods]
+        const filteredArray = array.filter(prod => parseInt(prod.price) <= price)
+        if(selectedCategories.length !== 0)
+        {
+            //@ts-ignore
+            const finalArray = filteredArray.filter(prod => selectedCategories?.includes(prod.category))
+            //@ts-ignore
+            setSelectedProducts(finalArray)
+        }
+        else
+        {
+            //@ts-ignore
+            setSelectedProducts(filteredArray)
+        }
+    }, [price, products, selectedCategories])
 
     let content
     if(isLoading || isLoadingCats) content = <ClipLoader />
     else if(isSuccess && isSuccessCats)
     {
-        const { ids, entities } = products
+        const { entities } = products
         const { ids: catsIds } = cats
 
-        const displayedProducts = ids.map(id => 
-            {
-                if(selectedCategories.length !== 0)
-                {
-                    //@ts-ignore
-                    if (selectedCategories.includes(entities[id].category)) 
-                    {
-                        return <Product key={id} selectedCategories={selectedCategories} userId={userId} product={entities[id]} />
-                    }
-                }
-                else 
-                {
-                    return <Product key={id} selectedCategories={selectedCategories} userId={userId} product={entities[id]} />
-                }
-            })
+        // const displayedProducts = ids.map(id => 
+        //     {
+        //         if(selectedCategories.length !== 0)
+        //         {
+        //             //@ts-ignore
+        //             if (selectedCategories.includes(entities[id].category)) 
+        //             {
+        //                 return <Product key={id} selectedCategories={selectedCategories} userId={userId} product={entities[id]} />
+        //             }
+        //         }
+        //         else 
+        //         {
+        //             return <Product key={id} selectedCategories={selectedCategories} userId={userId} product={entities[id]} />
+        //         }
+        //     })
+
+        //@ts-ignore
+        const displayedProducts = selectedProducts.map(prod => <Product key={prod._id} selectedCategories={selectedCategories} userId={userId} product={entities[prod._id]} />)
+
         const displayedCategories = catsIds.map(id => <Category key={id} userId={userId} category={id} select={setSelectedCategories} />)
         //console.log(selectedCategories)
         content = (
-            <div className='ProductsContainer'>
-                <div className='ProductsFiltersContainer'>
-                    <motion.div transition={{ duration: '0.05' }} animate={filter ? {width: ['10%', '40%', '60%', '80%', '100%']} : {width: ['100%', '80%', '60%', '40%', '10%']}} className={'ProductsFilters'} >
-                        { filter &&
-                            <div className='Categories'>
-                                <div 
-                                    onClick={() => setCategories(prev => !prev)} 
-                                    className='CategoriesMenu'
-                                >
-                                        Categories <FontAwesomeIcon icon={categories ? faAngleDown : faAngleUp } rotation={180} />
-                                </div>
-                                <motion.div transition={{ duration: '0.5' }} animate={categories ? {height: ['0vh', `${catsIds.length * 5}vh`]} : {height: [`${catsIds.length * 5}vh`, '0vh']}} className='CategoriesList'>
-                                    {displayedCategories}
-                                </motion.div>
-                            </div>
-                        }
-                        { filter &&
-                            <motion.div className='PriceFilterContainer'>
-                                <label>Price: ${price}</label>
-                                <input type='range' min={minPrice} max={maxPrice} value={price} onChange={(e) => setPrice(parseInt(e.target.value))} />
-                            </motion.div>
-                        }
-                    </motion.div>
-                    <motion.div className='ProductsFilterButton'>
-                        <button
-                            onClick={() => setFilter(prev => !prev)}
-                            name='menu'
-                        >
-                            <FontAwesomeIcon icon={filter ? faAngleDown : faAngleUp } rotation={90} />
-                        </button>
-                    </motion.div>
+            // <div className='ProductsContainer'>
+            //     <div className='ProductsFiltersContainer'>
+            //         <div className={'ProductsFilters'} >
+            //                 <div className='Categories'>
+            //                     <div 
+            //                         onClick={() => setCategories(prev => !prev)} 
+            //                         className='CategoriesMenu'
+            //                     >
+            //                             Categories <FontAwesomeIcon icon={categories ? faAngleDown : faAngleUp } rotation={180} />
+            //                     </div>
+            //                     <motion.div transition={{ duration: '0.5' }} animate={categories ? {height: ['0vh', `${catsIds.length * 5}vh`]} : {height: [`${catsIds.length * 5}vh`, '0vh']}} className='CategoriesList'>
+            //                         {displayedCategories}
+            //                     </motion.div>
+            //                 </div>
+            //                 <motion.div className='PriceFilterContainer'>
+            //                     <label>Price: ${price}</label>
+            //                     <input type='range' min={minPrice} max={maxPrice} value={price} onChange={(e) => setPrice(parseInt(e.target.value))} onMouseUp={handlePriceChange} />
+            //                 </motion.div>
+            //         </div>
+            //     </div>
+
+            //     {displayedProducts}
+            // </div>
+
+            <div className='MainContainer'>
+                <div className='SideMenuContainer'>
+                    <div className='CategoriesContainer'>
+                        {displayedCategories}
+                    </div>
+                    <div className='PriceContainer'>
+                        <label>Price: ${price}</label>
+                        <input type='range' min={minPrice} max={maxPrice} value={price} onChange={(e) => setPrice(parseInt(e.target.value))} onMouseUp={handlePriceChange} />
+                    </div>
                 </div>
-                <motion.div transition={{ duration: 0.8 }} animate={filter ? {width: ['100%', '80%']} : {width: ['80%', '100%']}} className='ProductsList'>
+                <div className='ProductsContainer'>
                     {displayedProducts}
-                </motion.div>
+                </div>
             </div>
         )
     }
