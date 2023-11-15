ccd5e000743b87b97914409668d30f0a90f04413
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2023-11-15 14:18:09 +0200

    Final

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery(
     {
-        baseUrl: 'http://localhost:3001',
+        baseUrl: 'https://njshopapi.onrender.com',
         credentials: 'include',
         prepareHeaders: (headers, { getState }) => 
         {
